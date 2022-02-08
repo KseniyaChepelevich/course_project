9051d512ef81f42a93de6ba92ba7a3b989a49578
KseniyaChepelevich <chepelevich.qamid.5@gmail.com>
2022-02-08 10:07:10 +0300

    added Page.jav

--- src/test/java/ru/netology/web/page/Page.java
+++ src/test/java/ru/netology/web/page/Page.java
@@ -1,4 +1,22 @@
 package ru.netology.web.page;
 
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+
 public class Page {
+    private SelenideElement heading = $("[id=root]");
+    private SelenideElement buttonBuy = $(".button__text").$(withText("Купить"));
+    private SelenideElement buttonBuyInCredit = $(".button__text").$(withText("Купить в кредит"));
+    private SelenideElement headingCardPayment = $(".heading").$(withText("Оплата по карте"));
+    private SelenideElement  cardNumberField = $(".input").$(withText("Номер карты"));
+    private SelenideElement fieldMonth = $(".input").$(withText("Месяц"));
+    private SelenideElement fieldYear = $(".input").$(withText("Год"));
+    private SelenideElement fieldOwner = $(".input").$(withText("Владелец"));
+    private SelenideElement fieldCVC = $(".input").$(withText("CVC/CVV"));
+    private SelenideElement buttonContinue = $(".button__text").$(withText("Продолжить"));
+
+    private SelenideElement headingCardCredit= $(".heading").$(withText("Кредит по данным карты"));
+
 }
